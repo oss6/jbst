0d23733276a8633ccd8a238d44410e73cfde867a
Ossama Edbali <ossedb@gmail.com>
2015-01-07 02:41:45 +0000

    Implemented height fns

--- jbst.js
+++ jbst.js
@@ -1,23 +1,47 @@
 ;(function (window, undefined) {
     
-    // Exception constructor
+// Exception constructor
+    
     function BSTException(message) {
         this.message = message;
     }
+
+// Private variables and functions    
+
+// Node constructor
     
-    // Node constructor (Empty constructor is null)
     function Node(value, left, right) {
         if (!(this instanceof Node)) {
             return new Node(value, left, right);
         }
         
         if (!(left instanceof Node && right instanceof Node) && (left !== null && right !== null))
-            return new BSTException('');
+            return new BSTException('Error! Not a valid node.');
         
         this.value = value;
         this.left = left;
         this.right = right;
     }
+    
+    Node.prototype = {
+        
+        height: function () {
+            var lh = 0,
+                rh = 0;
+            
+            return (function _aux (node, depth) {
+                if (!node) return -1;
+                
+                lh = _aux(node.left, lh);
+                rh = _aux(node.right, rh);
+                
+                return 1 + Math.max(lh, rh);
+            })(this);
+        }
+        
+    };
+    
+// BST constructor
     
     function BST(root) {
         this.root = root;   
@@ -25,23 +49,63 @@
     
     BST.prototype = {
         
-        height: function () {
+        size: function () {
             
         },
         
+        height: function () {
+            return this.root.height();
+        },
+        
         inOrder: function () {
-            return (function _aux (node) {
+            var ret = [];
+            
+            (function _aux (node) {
                 if (node) {
                     _aux(node.left);
-                    console.log(node.value);
+                    ret.push(node.value);
                     _aux(node.right);
                 }
             })(this.root);
+            
+            return ret;
+        },
+        
+        preOrder: function () {
+            var ret = [];
+            
+            (function _aux (node) {
+                if (node) {
+                    ret.push(node.value);
+                    _aux(node.left);
+                    _aux(node.right);
+                }
+            })(this.root);
+            
+            return ret;
+        },
+        
+        postOrder: function () {
+            var ret = [];
+            
+            (function _aux (node) {
+                if (node) {
+                    _aux(node.left);
+                    _aux(node.right);
+                    ret.push(node.value);
+                }
+            })(this.root);
+            
+            return ret;
+        },
+        
+        insert: function (val) {
+               
         }
         
     };
     
-    // Expose the module
+// Expose the module
     window.jbst = {
         'Node': Node,
         'BST': BST
