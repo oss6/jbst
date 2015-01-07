7a2227e7bdc4bc2b96ed29f633745d79a0fd15e8
Ossama Edbali <ossedb@gmail.com>
2015-01-07 02:01:28 +0000

    Implemented inOrder fn

--- jbst.js
+++ jbst.js
@@ -19,8 +19,31 @@
         this.right = right;
     }
     
+    function BST(root) {
+        this.root = root;   
+    }
+    
+    BST.prototype = {
+        
+        height: function () {
+            
+        },
+        
+        inOrder: function () {
+            return (function _aux (node) {
+                if (node) {
+                    _aux(node.left);
+                    console.log(node.value);
+                    _aux(node.right);
+                }
+            })(this.root);
+        }
+        
+    };
+    
     // Expose the module
     window.jbst = {
-        'Node': Node
+        'Node': Node,
+        'BST': BST
     };
 })(window);
